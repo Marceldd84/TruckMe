0efd9311ac867a2a07fe16957dc1986ba77938f1
Marceldd84 <marceldd@gmail.com>
2018-11-24 14:02:40 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularJsTruck';
+  lat = 51.678418;
+  lng = 7.809007;
+
+  markers: marker[] = [
+    {
+      lat: 25.694055,
+      lng: -80.321886,
+      label: 'A',
+      content: 'asdasd'
+    },
+    {
+      lat: 25.693233,
+      lng: -80.319740,
+      label: 'J',
+      content: 'Jordy el piti pua'
+    },
+    {
+      lat: 25.692856,
+      lng: -80.317766,
+      label: 'C',
+      content: 'xsdad'
+    }
+  ];
+
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+this.calculateDistance();
+
+      });
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
+  calculateDistance() {
+    let p = 0.017453292519943295;    // Math.PI / 180
+    let c = Math.cos;
+    // tslint:disable-next-line:max-line-length
+    let a = 0.5 - c((this.lat - this.markers[0].lat) * p) / 2 + c(this.markers[0].lat * p) * c((this.lat) * p) * (1 - c(((this.lng - this.markers[0].lng) * p))) / 2;
+    let dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
+    return dis;
+
+  }
+}
+
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  content?: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { CustomMaterialModule } from './customMaterialModule';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBj3slnKzGSe-KAd0Ss-r3qKU1icptcJUc'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
